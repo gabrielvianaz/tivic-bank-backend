624e8f6b94a620d74ea3cbcf7b091ed858e9458b
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-03-29 22:29:01 -0300

    Setup inicial do projeto

---
+++ src/main/java/software/gabriel/tivic/bank/backend/TivicBankBackendApplication.java
@@ -0,0 +1,13 @@
+package software.gabriel.tivic.bank.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TivicBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TivicBankBackendApplication.class, args);
+	}
+
+}
